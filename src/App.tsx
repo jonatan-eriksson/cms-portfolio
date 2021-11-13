fee96377c75eb59f2b0360f333710266a9c94b7c
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-14 00:41:59 +0100

    Add a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <div className="App min-h-screen flex flex-col bg-gray-900 text-gray-300 space-y-8 text-center">
+      <div className="App min-h-screen bg-gray-900 text-gray-300 flex flex-col">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="about" element={<h1>About</h1>} />
-          <Route path="projects" element={<h1>Projects</h1>} />
-          <Route path="contact" element={<h1>Contact</h1>} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
+        <div className="container mx-auto px-4 flex-1 flex">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<h1>About</h1>} />
+            <Route path="projects" element={<h1>Projects</h1>} />
+            <Route path="contact" element={<h1>Contact</h1>} />
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </div>
       </div>
     </Router >
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [image, setImage] = useState<any>({});
+
+  useEffect(() => {
+    const fetchPage = async () => {
+      const res = await axios.get('wp-json/wp/v2/pages?slug=home');
+      const page = res.data[0];
+      console.log(page);
+
+      setTitle(page.acf.title);
+      setDescription(page.acf.description);
+      setImage(page.acf.image);
+    }
+    fetchPage();
+  }, []);
+
+  return (
+
+    <div className="self-center mx-auto">
+      <h1 className="font-semibold text-gray-200 text-4xl md:text-5xl lg:text-6xl pt-3">{title}</h1>
+      <p className="font-light text-gray-400 text-xl pt-3">{description}</p>
+      <Link to="/contact" className="block sm:inline-block mt-12 py-3 px-10 font-semibold text-xl sm:text-2xl text-center sm:text-left text-teal-300 border border-teal-300 hover:bg-teal-300 hover:bg-opacity-10 transition-colors rounded">Let's talk!</Link>
+    </div >
+
+  );
+}
+
+export default Home;
