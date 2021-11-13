823c69a43c7c2f71fc867cbb14b8a900104fda94
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-13 22:06:25 +0100

    Add routes for each page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ function App() {
     <Router>
       <div className="App min-h-screen flex flex-col bg-gray-900 text-gray-300 space-y-8 text-center">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="projects" element={<h1>Projects</h1>} />
+          <Route path="contact" element={<h1>Contact</h1>} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
       </div>
     </Router >
   );
