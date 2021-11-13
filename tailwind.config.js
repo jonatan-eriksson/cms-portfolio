f4fd6c2910950a0871f5787ee3f7bfaa69f47c29
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-13 21:54:37 +0100

    Add npm packages and tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  // purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      gray: colors.trueGray,
+      teal: colors.teal,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
