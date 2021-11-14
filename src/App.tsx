9f3147cf65685cb99ad6253a7f56065406dce2dc
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-14 15:04:16 +0100

    Add About page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <div className="container mx-auto px-4 flex-1 flex">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="about" element={<h1>About</h1>} />
+            <Route path="about" element={<About />} />
             <Route path="projects" element={<h1>Projects</h1>} />
             <Route path="contact" element={<h1>Contact</h1>} />
             <Route path="*" element={<h1>404</h1>} />
---
+++ src/pages/About.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const About = () => {
+  const [loading, setLoading] = useState(true);
+  const [description, setDescription] = useState<string>("");
+
+  useEffect(() => {
+    const fetchPage = async () => {
+      const res = await axios.get('wp-json/wp/v2/pages?slug=about');
+      const page = res.data[0];
+      console.log(page);
+
+      setDescription(page.acf.description);
+    }
+    fetchPage();
+  }, []);
+
+  return (
+    <div className="mt-6">
+      <h2 className="text-4xl font-medium text-gray-200 mb-5">About me</h2>
+      <p className="text-gray-400">
+        {description}
+      </p>
+    </div >
+  );
+}
+
+export default About;
