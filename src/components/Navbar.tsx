0b0fb87906c628a99da04e093e2804cf8f4826a8
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-14 15:37:06 +0100

    Update navbar css classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   }, [])
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-6">
+    <nav className="container mx-auto flex items-center justify-between flex-wrap p-6">
       <div className="text-gray-200 mr-6">
         <NavLink className="font-semibold text-xl tracking-tight" to="/">
           {title}
@@ -46,7 +46,7 @@ const Navbar = () => {
       <div className={"w-full md:block md:w-auto " + (isOpen ? "" : "hidden")}>
         <div className="text-sm md:flex-grow">
           {menuItems && menuItems.map((item: any, idx: number) => (
-            <NavLink key={idx} className="block mt-4 md:inline-block md:mt-0 text-gray-200 hover:text-teal-300 mr-4" to={`/${item.title.toLowerCase() === "home" ? "" : item.title.toLowerCase()}`}>
+            <NavLink key={idx} className="block mt-4 md:inline-block md:mt-0 md:mx-2 text-gray-200 hover:text-teal-300" to={`/${item.title.toLowerCase() === "home" ? "" : item.title.toLowerCase()}`}>
               {item.title}
             </NavLink>
           ))}
