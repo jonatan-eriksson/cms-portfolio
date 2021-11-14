84fd1e725a67f1a59467618a79ef0169291ed05d
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-11-14 17:24:05 +0100

    Add github and external link icons to project items

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,3 +1,5 @@
+import { VscGithubAlt, VscLinkExternal } from "react-icons/vsc";
+
 const ProjectItem = ({ project }: any) => {
   return (
     <div className="max-w md:max-w-2xl lg:max-w-md xl:max-w-sm rounded overflow-hidden shadow-lg bg-gray-800">
@@ -7,10 +9,23 @@ const ProjectItem = ({ project }: any) => {
         <p className="text-gray-300 text-base">{project.acf.description}</p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        {project.acf.tags.map((tag: any) => (
-          <span className="inline-block bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-300 mr-2 mb-2">{tag.name}</span>
+        {project.acf.tags && project.acf.tags.map((tag: any, idx: number) => (
+          <span key={idx} className="inline-block bg-gray-700 rounded px-2 py-1 text-sm text-gray-300 mr-2 mb-2">{tag.name}</span>
         ))}
       </div>
+      <div className="px-6 pt-4 pb-2 flex gap-2">
+        {project.acf.githuburl && (
+          <a className="text-2xl hover:text-teal-300" href={project.acf.githuburl} target="_blank" rel="noopener noreferrer">
+            <VscGithubAlt />
+          </a>
+        )}
+        {project.acf.url && (
+          <a className="text-2xl hover:text-teal-300" href={project.acf.url} target="_blank" rel="noopener noreferrer">
+            <VscLinkExternal />
+          </a>
+        )}
+      </div>
+
     </div>
   );
 }
