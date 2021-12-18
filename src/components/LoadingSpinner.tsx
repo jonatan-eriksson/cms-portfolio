1b6b4a14e54e0530187c16fc42ec3af25fe26e6d
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-12-19 00:47:17 +0100

    Add LoadingSpinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+const LoadingSpinner = () => (
+  <div
+    style={{ borderTopColor: 'transparent' }}
+    className="self-center mx-auto w-16 h-16 border-4 border-teal-300 border-solid rounded-full animate-spin"
+  />
+);
+
+export default LoadingSpinner;
